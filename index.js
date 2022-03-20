0597f720da19c480ccead0c2aa49177b3c2ca914
DIPJOY PAUL ANTIK <antikpaul1994@gmail.com>
2022-03-20 22:32:02 +0600

    Store and Reducer

---
+++ index.js
@@ -0,0 +1,17 @@
+const redux = require('redux')
+// console.log(redux);
+
+// state
+const initState = {
+    num : 0
+}
+
+// Reducer
+const rootReducer = (state = initState  , action) => {
+    return state;
+}
+
+// Store
+const store = redux.createStore( rootReducer);
+console.log(store.getState());
+
