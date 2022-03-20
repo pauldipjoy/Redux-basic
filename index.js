5783a9bb3fd9863ec3922de33873e97797756eb4
DIPJOY PAUL ANTIK <antikpaul1994@gmail.com>
2022-03-20 23:33:25 +0600

    Dispatching Actions

--- index.js
+++ index.js
@@ -8,10 +8,38 @@ const initState = {
 
 // Reducer
 const rootReducer = (state = initState  , action) => {
+    if(action.type === 'INC_NUM'){
+        return {
+            ...state,
+            num : state.num + 1
+        }
+    }
+    if(action.type === 'ADD_NUM'){
+        return {
+            ...state,
+            num: state.num + action.value
+        }
+    }
     return state;
 }
 
 // Store
 const store = redux.createStore( rootReducer);
 console.log(store.getState());
+
+
+
+
+// Dispatching Action-
+store.dispatch({
+    type : 'INC_NUM'
+});
+
+store.dispatch({
+    type : 'ADD_NUM',
+    value : 7
+});
+
+console.log(store.getState());
+
 
